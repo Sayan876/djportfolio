7a848e71d26702d91bd6deb76773cfdfe657b73c
Sayan876 <120472214+Sayan876@users.noreply.github.com>
2026-08-16 14:03:57 +0530

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -144,7 +144,7 @@ const Experience = () => {
                     </h3>
 
                     <p className="mt-1 text-sm font-medium text-muted-foreground">
-                      IT Service Desk Engineer
+                      IT Service Desk Engineer | Technical Support Engineer
                     </p>
                   </div>
                 </motion.div>
